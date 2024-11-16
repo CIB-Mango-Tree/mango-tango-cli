066067813d078e4b9fb9aea4df26b061f77a5a47
Kristijan Armeni <kristijan.armeni@gmail.com>
2024-11-16 09:56:50 -0500

    feat: hastags analyzer (#7)
    * initial commit, playground
    
    * update report with example output
    
    * update, typos etc.
    
    * add hashtags to analyzers suite
    
    * rewrite gini()
    
    * [FIX] add comma
    
    * initial commit, interface.py and main.py
    
    * move the gini() function out
    
    * cleanup
    
    * update name attribute of AnalyzerOutput
    
    * cleanup files
    
    * remove hashtags.py
    
    * [FIX, ENH] explode hashtags column, use Counter in gini()
    
    * [FIX] fix AnalyzerDeclaration import statement
    
    * [FIX] replace input argument in main()  with PrimaryAnalyzerContext
    
    * rename folder hashtag --> hashtags
    
    * [FIX] update import module name
    
    * [FIX] use OUTPUT_GINI constant as key in output arg of hashtags.main.py
    
    * Update analyzers/hashtags/main.py
    
    use `.write_parquet()` for output in `main()`
    
    Co-authored-by: soul-codes <40335030+soul-codes@users.noreply.github.com>
    
    * Update analyzers/hashtags/interface.py
    
    Co-authored-by: soul-codes <40335030+soul-codes@users.noreply.github.com>
    
    * Update analyzers/hashtags/interface.py
    
    add constants for output cols
    
    Co-authored-by: soul-codes <40335030+soul-codes@users.noreply.github.com>
    
    * Update analyzers/hashtags/main.py
    
    Co-authored-by: soul-codes <40335030+soul-codes@users.noreply.github.com>
    
    * Update analyzers/hashtags/main.py
    
    [ENH] fix and use constants in output
    
    Co-authored-by: soul-codes <40335030+soul-codes@users.noreply.github.com>
    
    * [MAINT] use constants in main()
    
    Co-authored-by: soul-codes <40335030+soul-codes@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: soul-codes <40335030+soul-codes@users.noreply.github.com>

---
+++ analyzers/hashtags/__init__.py
@@ -0,0 +1,8 @@
+from analyzer_interface import AnalyzerDeclaration
+from .interface import interface
+from .main import main
+
+hashtags = AnalyzerDeclaration(
+  interface=interface,
+  main=main
+)
---
+++ analyzers/hashtags/main.py
@@ -0,0 +1,84 @@
+import polars as pl
+from itertools import accumulate
+from .interface import (
+    COL_AUTHOR_ID,
+    COL_TIME,
+    COL_HASHTAGS,
+    OUTPUT_GINI,
+    OUTPUT_COL_COUNT,
+    OUTPUT_COL_GINI,
+    OUTPUT_COL_HASHTAGS
+)
+from collections import Counter
+from analyzer_interface.context import PrimaryAnalyzerContext
+
+# let's look at the hashtags column
+COLS_ALL = [COL_AUTHOR_ID, COL_TIME, COL_HASHTAGS]
+
+NULL_CHAR = "[]" # this is taken as the null character for hashtags
+
+def gini(x):
+    """
+    Parameters
+    ----------
+    x : list[str]
+        List of values for which to compute the Gini coefficient
+
+    Returns
+    -------
+    float
+        Gini coefficient
+    """
+    x_counts = Counter(x).values()
+
+    sorted_x = sorted(x_counts)
+    n = len(sorted_x)
+    cumx = list(accumulate(sorted_x))
+
+    return (n + 1 - 2 * sum(cumx) / cumx[-1]) / n
+
+
+def main(context: PrimaryAnalyzerContext):
+
+    input_reader = context.input()
+    df_input = input_reader.preprocess(
+        pl.read_parquet(input_reader.parquet_path)
+    )
+
+    # assign None to messages with no hashtags
+    df_input = df_input.with_columns(
+        pl.when(pl.col(COL_HASHTAGS) == NULL_CHAR)
+        .then(None)
+        .otherwise(pl.col(COL_HASHTAGS)
+                .str.strip_chars("[]")
+                .str.replace_all("'", "")
+                .str.replace_all(" ", "")
+                .str.split(",")) # split hashtags into List[str]
+        .name.keep()
+    )
+
+    # select columns
+    df_input = df_input.select(pl.col(COLS_ALL))
+
+    df_agg = (
+        df_input.filter(pl.col(COL_HASHTAGS).is_not_null())
+        .select(
+            pl.col(COL_TIME),
+            pl.col(COL_HASHTAGS),
+        )
+        .sort(COL_TIME)
+        .group_by_dynamic(COL_TIME, every="1h") # this could be a parameter
+        .agg(
+            pl.col(COL_HASHTAGS).explode().alias(OUTPUT_COL_HASHTAGS),
+            pl.col(COL_HASHTAGS).explode().count().alias(OUTPUT_COL_COUNT),
+            pl.col(COL_HASHTAGS).explode().map_elements(
+                lambda x: gini(x.to_list()), 
+                return_dtype=pl.Float32,
+                returns_scalar=True).alias(OUTPUT_COL_GINI),
+            )
+    )
+
+    print("Output preview:")
+    print(df_agg.head())
+
+    df_agg.write_parquet(context.output(OUTPUT_GINI).parquet_path)
