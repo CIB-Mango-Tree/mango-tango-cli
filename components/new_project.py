7fcfa9ef19aaf01647a34ebe7d73353205cc37f7
soul-codes <reach.soul.codes@gmail.com>
2024-12-11 17:01:47 -0500

    feat: 🎸 customizable CSV import options

--- components/new_project.py
+++ components/new_project.py
@@ -2,12 +2,13 @@ import os
 import tempfile
 from typing import Optional
 
-from importing import IImporterPreload, Importer, importers
+from importing import ImporterSession, Importer, importers
 from storage import Project, Storage
 from terminal_tools import draw_box, prompts, wait_for_key
-from terminal_tools.inception import TerminalContext
-
+from terminal_tools.inception import TerminalContext, Scope
+import polars as pl
 from .utils import input_preview
+from traceback import format_exc
 
 
 def new_project(context: TerminalContext, storage: Storage):
@@ -18,37 +19,10 @@ def new_project(context: TerminalContext, storage: Storage):
       print("Canceled")
       return wait_for_key(True)
 
-    print(f"Selected file: {selected_file}")
-    import_spec: Optional[tuple[Importer, IImporterPreload]] = None
-    import_errors: list[tuple[Importer, Exception]] = []
-
-    for importer in importers:
-      if not importer.sniff(selected_file):
-        continue
-
-      try:
-        preload = importer.preload(selected_file, 100)
-        import_spec = (importer, preload)
-      except Exception as e:
-        import_errors.append((importer, e))
-        continue
-
-      print(f"Input loaded as a {importer.name} file.")
-      break
-
-    if import_spec is None:
-      print("We couldn't open this file. It is likely that the file is not supported.")
-      if prompts.confirm("Would you like to see the errors?", default=False):
-        for importer, e in import_errors:
-          print(f"Error trying attempting to load as {importer.name}: {e}")
-      return wait_for_key(True)
-
-    importer, preload = import_spec
-
-  with context.nest(draw_box("2. Data preview", padding_lines=0)):
-    input_preview(preload.get_preview_dataframe())
-    confirm_preview = prompts.confirm("Is this correct?", default=True)
-    if not confirm_preview:
+  with context.nest(draw_box("2. Import Options", padding_lines=0)) as scope:
+    importer: Optional[ImporterSession] = importer_flow(
+      selected_file, importers, scope)
+    if importer is None:
       print("Canceled")
       return wait_for_key(True)
 
@@ -63,7 +37,7 @@ def new_project(context: TerminalContext, storage: Storage):
   with context.nest(draw_box("4. Import", padding_lines=0)):
     print("Please wait as the dataset is imported...")
     with tempfile.NamedTemporaryFile() as temp_file:
-      importer.import_data(selected_file, temp_file.name, preload)
+      importer.import_as_parquet(temp_file.name)
       project = storage.init_project(
         display_name=project_name, input_temp_file=temp_file.name)
 
@@ -73,3 +47,142 @@ def new_project(context: TerminalContext, storage: Storage):
       id=project.id,
       display_name=project.display_name
     )
+
+
+def importer_flow(
+  input_file: str,
+  importers: list[Importer[ImporterSession]],
+  scope: Scope
+):
+  suggested_importer: Optional[Importer[ImporterSession]] = None
+  for importer in importers:
+    if importer.suggest(input_file):
+      suggested_importer = importer
+      break
+
+  importer = suggested_importer
+  is_first_time_importer_infer_error = True
+
+  import_session = None
+  import_preview = None
+  import_session_error = None
+  import_session_traceback = None
+  while True:
+    scope.refresh()
+    print(f"Importing {input_file}")
+    print("")
+
+    if not import_session:
+      if not importer:
+        if not is_first_time_importer_infer_error:
+          print("Could not figure out how to import this file.")
+          is_first_time_importer_infer_error = False
+
+        print("Please select a format to open the file as.")
+        importer = prompts.list_input(
+          "Select a format",
+          choices=[(importer.name, importer) for importer in importers]
+        )
+
+      if importer is None:
+        return None
+
+    if not import_session:
+      try:
+        import_session = importer.init_session(input_file)
+      except Exception as e:
+        import_session_error = e
+        import_session_traceback = format_exc()
+
+    if import_session:
+      import_preview = None
+      import_session_error = None
+      import_session_traceback = None
+      try:
+        import_preview = import_session.load_preview(1)
+      except Exception as e:
+        import_session_error = e
+        import_session_traceback = format_exc()
+
+    if import_session:
+      if import_session_error:
+        print("Could not load a preview of the data.")
+        print(f"❌ Error:\n{indent_error(str(import_session_error))}")
+        print(f"Importing as {importer.name} with these options:")
+        import_session.print_config()
+        print("")
+      else:
+        print(f"Importing as {importer.name} with these options:")
+        import_session.print_config()
+        print("")
+        print("The data has these columns:")
+        print("(Each column should be listed in its own line)")
+        for column in import_preview.columns:
+          print(f"  [{column}]")
+        print("")
+    else:
+      print("Could not figure out how to import this file as {importer.name}.")
+
+    import_action = prompts.list_input(
+      "What would you like to do?",
+      choices=[
+        *([
+            ("Import this file", "import"),
+          ] if import_preview is not None else []),
+        *([
+            ("Modify the import options", "modify")
+          ] if import_session else [
+            ("Manually configure the import options", "manual")
+          ]),
+        *([
+            ("View the import error", "view_error")
+          ] if import_session_error else []),
+        *([
+          ("Open this file as a different format", "different_format")
+          ] if len(importers) > 1 else []),
+        ("Cancel", None)
+      ]
+    )
+
+    if import_action is None:
+      return None
+
+    if import_action == "import":
+      assert import_session is not None
+      return import_session
+
+    if import_action == "different_format":
+      import_session = None
+      importer = None
+      continue
+
+    if import_action == "manual":
+      import_session = importer.manual_init_session(input_file)
+      continue
+
+    if import_action == "modify":
+      assert import_session is not None
+
+      def reset_screen_for_customize_session(session=import_session):
+        scope.refresh()
+        print(f"Importing {input_file}")
+        print("")
+        print(f"Importing as {importer.name} with these options:")
+        session.print_config()
+
+      import_session = importer.modify_session(
+        input_file, import_session, reset_screen_for_customize_session
+      )
+      if import_session is None:
+        continue
+
+    if import_action == "view_error":
+      assert import_session_error is not None
+      assert import_session_traceback is not None
+      print(f"Error:\n{indent_error(str(import_session_traceback))}")
+      wait_for_key(True)
+      continue
+
+
+def indent_error(err: str):
+  return "\n".join([" ! " + line for line in err.split("\n")]) + "\n"
--- importing/csv.py
+++ importing/csv.py
@@ -1,53 +1,211 @@
-from csv import Sniffer, Dialect
-from typing import Any
+from csv import Sniffer
+from typing import Optional, Callable
 
 import polars as pl
 from pydantic import BaseModel
 
-from .importer import IImporterPreload, Importer
+from .importer import ImporterSession, Importer
+import terminal_tools.prompts as prompts
 
 
-class CSVImporter(Importer["CSVPreload"]):
+class CSVImporter(Importer["CsvImportSession"]):
   @property
   def name(self) -> str:
     return "CSV"
 
-  def sniff(self, input_path: str) -> bool:
+  def suggest(self, input_path: str) -> bool:
     return input_path.endswith(".csv")
 
-  def preload(self, input_path: str, n_records: int):
+  def init_session(self, input_path: str):
     with open(input_path, "r", encoding="utf8") as file:
       dialect = Sniffer().sniff(file.read(65536))
-    df = pl.read_csv(
-      input_path, **self._get_csv_args(dialect), n_rows=n_records
-    )
-    print(dialect)
-    return CSVPreload(preview_df=df, dialect=dialect)
 
-  def import_data(self, input_path: str, output_path: str, preload: "CSVPreload") -> pl.DataFrame:
-    dialect: Dialect = preload.dialect
-    lazyframe = pl.scan_csv(
-      input_path, **self._get_csv_args(dialect)
+    return CsvImportSession(
+      input_file=input_path,
+      separator=dialect.delimiter,
+      quote_char=dialect.quotechar,
+      has_header=True
     )
-    lazyframe.sink_parquet(output_path)
+
+  def manual_init_session(self, input_path: str):
+    separator = self._separator_option(None)
+    if separator is None:
+      return None
+
+    quote_char = self._quote_char_option(None)
+    if quote_char is None:
+      return None
+
+    has_header = self._header_option(None)
+    if has_header is None:
+      return None
+
+    return CsvImportSession(
+      input_file=input_path,
+      separator=separator,
+      quote_char=quote_char,
+      has_header=has_header
+    )
+
+  def modify_session(
+    self,
+    input_path: str,
+    import_session: "CsvImportSession",
+    reset_screen: Callable[[], None]
+  ):
+    is_first_time = True
+    while True:
+      reset_screen(import_session)
+      action = prompts.list_input(
+        "What would you like to change?",
+        choices=[
+          ("Column separator", "separator"),
+          ("Quote character", "quote_char"),
+          ("Header", "header"),
+          ("Done. Use these options.", "done"),
+        ],
+        default=None if is_first_time else "done"
+      )
+      is_first_time = False
+      if action is None:
+        return None
+
+      if action == "done":
+        return import_session
+
+      if action == "separator":
+        separator = self._separator_option(import_session.separator)
+        if separator is None:
+          continue
+        import_session.separator = separator
+
+      if action == "quote_char":
+        quote_char = self._quote_char_option(import_session.quote_char)
+        if quote_char is None:
+          continue
+        import_session.quote_char = quote_char
+
+      if action == "header":
+        has_header = self._header_option(import_session.has_header)
+        if has_header is None:
+          continue
+        import_session.has_header = has_header
 
   @staticmethod
-  def _get_csv_args(dialect: Dialect) -> dict[str, str]:
-    return {
-      "separator": dialect.delimiter,
-      "quote_char": dialect.quotechar,
-      "ignore_errors": True,
-      "has_header": True,
-      "truncate_ragged_lines": True,
-    }
+  def _separator_option(previous_value: Optional[str]) -> Optional[str]:
+    input: Optional[str] = prompts.list_input(
+      "Select the column separator",
+      choices=[
+        ("comma (,)", ","),
+        ("semicolon (;)", ";"),
+        ("Pipe (|)", "|"),
+        ("Tab", "\t"),
+        ("Other", "other"),
+      ],
+      default=(
+        previous_value
+        if previous_value in [",", ";", "\t"]
+        else "other" if previous_value is not None
+        else None
+      )
+    )
+    if input is None:
+      return None
+    if input != "other":
+      return input
+
+    input = prompts.text("Enter the separator")
+    if input is None:
+      return None
+    input = input.strip()
+    if len(input) == 0:
+      return None
+
+  @staticmethod
+  def _quote_char_option(previous_value: Optional[str]) -> Optional[str]:
+    input: Optional[str] = prompts.list_input(
+      "Select the quote character",
+      choices=[
+        ("Double quote (\")", '"'),
+        ("Single quote (')", "'"),
+        ("Other", "other"),
+      ],
+      default=(
+        previous_value
+        if previous_value in ['"', "'"]
+        else "other" if previous_value is not None
+        else None
+      )
+    )
+    if input is None:
+      return None
+    if input != "other":
+      return input
+
+    input = prompts.text("Enter the quote character")
+    if input is None:
+      return None
+    input = input.strip()
+    if len(input) == 0:
+      return None
+
+  def _header_option(self, previous_value: Optional[bool]) -> Optional[bool]:
+    return prompts.list_input(
+      "Does the file have a header?",
+      choices=[
+        ("Yes", True),
+        ("No", False),
+      ],
+      default=previous_value
+    )
 
 
-class CSVPreload(IImporterPreload, BaseModel):
-  preview_df: pl.DataFrame
-  dialect: Any
+class CsvImportSession(ImporterSession, BaseModel):
+  input_file: str
+  separator: str
+  quote_char: str
+  has_header: bool = True
 
-  class Config:
-    arbitrary_types_allowed = True
+  def print_config(self):
+    def present_separator(value: str) -> str:
+      if value == "\t":
+        return "(Tab)"
+      if value == " ":
+        return "(Space)"
+      if value == ",":
+        return "(Comma ,)"
+      if value == ";":
+        return "(Semicolon ;)"
+      if value == "'":
+        return "(Single quote ')"
+      if value == '"':
+        return "(Double quote \")"
+      if value == "|":
+        return "(Pipe |)"
+      return value
 
-  def get_preview_dataframe(self) -> pl.DataFrame:
-    return self.preview_df
+    print(f"- Column separator: {present_separator(self.separator)}")
+    print(f"- Quote character: {present_separator(self.quote_char)}")
+    print(f"- First row is header: {'yes' if self.has_header else 'no'}")
+
+  def load_preview(self, n_records: int) -> pl.DataFrame:
+    return pl.read_csv(
+      self.input_file,
+      separator=self.separator,
+      quote_char=self.quote_char,
+      has_header=self.has_header,
+      n_rows=n_records,
+      truncate_ragged_lines=True,
+      ignore_errors=True,
+    )
+
+  def import_as_parquet(self, output_path: str) -> None:
+    lazyframe = pl.scan_csv(
+      self.input_file,
+      separator=self.separator,
+      quote_char=self.quote_char,
+      has_header=self.has_header,
+      truncate_ragged_lines=True,
+      ignore_errors=True,
+    )
+    lazyframe.sink_parquet(output_path)
