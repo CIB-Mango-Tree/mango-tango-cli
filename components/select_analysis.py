b87d14a71b0e3806d86989a2bdc470bb3823106d
soul-codes <reach.soul.codes@gmail.com>
2024-12-20 16:59:56 -0500

    fix: 🐛 improve analysis selector label

--- components/select_analysis.py
+++ components/select_analysis.py
@@ -1,6 +1,6 @@
+from datetime import datetime
 from typing import Optional
 
-from analyzer_interface import AnalyzerDeclaration
 from analyzers import suite
 from storage import AnalysisModel, Project, Storage
 from terminal_tools import prompts, wait_for_key
@@ -10,10 +10,11 @@ from terminal_tools.inception import TerminalContext
 def select_analysis(
     context: TerminalContext, storage: Storage, project: Project
 ) -> Optional[AnalysisModel]:
+    now = datetime.now()
     analysis_options = sorted(
         [
             (
-                f"{analysis.display_name} ({analysis.create_time() or 'unknown'})",
+                analysis_label(analysis, now),
                 analysis,
             )
             for analysis in storage.list_project_analyses(project.id)
@@ -34,3 +35,40 @@ def select_analysis(
         ],
     )
     return option
+
+
+def analysis_label(analysis: AnalysisModel, now: datetime) -> str:
+    create_time = analysis.create_time()
+    timestamp_suffix = (
+        " (" + present_timestamp(create_time, now) + ")"
+        if create_time is not None
+        else ""
+    )
+    return f"{analysis.display_name}{timestamp_suffix}"
+
+
+def present_timestamp(timestamp: datetime, now: datetime):
+    return timestamp.strftime("%Y-%m-%d %H:%M:%S")
+
+
+def present_timestamp(d: datetime, now: datetime):
+    diff = now - d
+    s = diff.seconds
+    if diff.days > 7 or diff.days < 0:
+        return d.strftime("%d %b %y")
+    elif diff.days == 1:
+        return "1 day ago"
+    elif diff.days > 1:
+        return "{} days ago".format(diff.days)
+    elif s <= 1:
+        return "just now"
+    elif s < 60:
+        return "{} seconds ago".format(s)
+    elif s < 120:
+        return "1 minute ago"
+    elif s < 3600:
+        return "{} minutes ago".format(s // 60)
+    elif s < 7200:
+        return "1 hour ago"
+    else:
+        return "{} hours ago".format(s // 3600)
